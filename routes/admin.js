68b9fe41736d91b57d6f1aaa1991acee4e4f7260
rahoofnazer <rahoofnazer@gmail.com>
2020-12-14 17:35:47 +0530

    design dashboard admin

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('admin/admin-login', { title: 'sdfdf' });
 });
 
+router.post('/login',(req,res)=>{
+  res.render('admin/admin-dashboard')
+})
 module.exports = router;
